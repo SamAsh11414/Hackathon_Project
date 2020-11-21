797491bb0840657f7e23702b5e190c46de87ee51
SamOutabrae <59368114+SamOutabrae@users.noreply.github.com>
2020-11-21 13:06:31 -0500

    Update snopes.js
    Made getTrueFalseValue return one of seven different values based on the rating of the statement.

--- snopes.js
+++ snopes.js
@@ -18,8 +18,38 @@ function searchSnopes(searchString) {
     return inIframe(url, iframe => iframe.contentDocument.querySelector('div.ais-hits--item'));
 }
 
+// TODO : Return all types of rating
 function getTrueFalseValue(searchString) {
+    var truefalse;
     const url = 'https://www.sonpes.com/?s=' + searchString.replaceAll(" ", "+");
     const topResultLink = inIframe(url, iframe => iframe.contentDocument.querySelector('.search-entry links').href);
-    return inIframe(topResultLink, iframe => if(iframe.contentDocument.classList)
+    return inIframe(topResultLink, iframe => {
+        if(iframe.contentDocument.querySelector('.rating-label-true')) {
+            return "true";
+        }
+
+        if(iframe.contentDocument.querySelector('.rating-label-mostly-true')) {
+            return "mostly-true";
+        }
+
+        if(iframe.contentDocument.querySelector('.rating-label-mixture')) {
+            return "mixture";
+        }
+
+        if(iframe.contentDocument.querySelector('.rating-label-mostly-false')) {
+            return "mostly-false";
+        }
+
+        if(iframe.contentDocument.querySelector('.rating-label-false')) {
+            return "false";
+        }
+
+        if(iframe.contentDocument.querySelector('.rating-label-unproven')) {
+            return "unproven";
+        }
+
+        if(iframe.contentDocument.querySelector('.rating-label-outdated')) {
+            return "outdated";
+        }
+    });
 }
