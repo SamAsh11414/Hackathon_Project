425a250fd22a0debf030a688870f243a9190a191
Linus Liang <LinusLiang@protonmail.com>
2020-11-22 06:52:05 -0500

    Add a bunch of utils (you'll see why later)

--- utils/utils.js
+++ utils/utils.js
@@ -1,16 +1,53 @@
+function throttle(fn, milli) {
+    return (...args) => {
+        
+    }
+}
+
+// this assumes that fn will not modify args in anyway; fn is functional
+function memorize(fn, thisVal) {
+    const memorization = new Map();
+    return (...args) => 
+        memorization.get(args) || memorization.set(args, fn.apply(thisVal, args)).get(args);
+}
+
+
 function getText(url) {
     return fetch(browser.runtime.getURL(url)).then(res => res.text());
 }
 
+function genDocumentFrag(string) {
+    const template = document.createElement('template');
+    template.innerHTML = string;
+    return template.content;
+}
+
+function getDocumentFrag(url) {
+    return getText(url).then(genDocumentFrag);
+}
+
+const memGetDocumentFrag = memorize(getDocumentFrag, this);
+const memGetText = memorize(getText, this);
+const memGetStyle = memorize(text => genDocumentFrag('<style>' + text + '</style>'), this);
+
 function setShadowHTML(shadow, htmlURL) {
-    return getText(htmlURL).then(text => (shadow.innerHTML += text, Promise.resolve(shadow)));
+    return memGetDocumentFrag(htmlURL).then(text => {
+        shadow.appendChild(text); 
+        return Promise.resolve(shadow);
+    });
 }
 
 function setShadowCSS(shadow, cssURL) {
-    return getText(cssURL).then(text => (shadow.innerHTML += ('<style>' + text + '</style>'), Promise.resolve(shadow)));
+    return memGetText(cssURL).then(text => {
+        shadow.appendChild(memGetStyle(text)); 
+        return Promise.resolve(shadow);
+    });
 }
 
 function setShadow(obj) {
     return setShadowHTML(obj.shadow, obj.html)
-        .then(shadow => (setShadowCSS(shadow, obj.css), Promise.resolve(shadow)));
+        .then(shadow => {
+            setShadowCSS(shadow, obj.css);
+            return Promise.resolve(shadow)
+        });
 }
