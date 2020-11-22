9a101225b3ef3b89b7b10fac891dff9487cc5649
SamOutabrae <59368114+SamOutabrae@users.noreply.github.com>
2020-11-22 11:44:39 -0500

    Formatted return statements.

--- utils/snopes.js
+++ utils/snopes.js
@@ -23,26 +23,26 @@ function searchSnopes(searchString) {
     return inIframe(url, iframe => iframe.contentDocument.querySelector('div.ais-hits--item'));
 }
 
-// TODO : Return all types of rating
 function getTrueFalseValue(searchString) {
     const url = 'https://www.sonpes.com/?s=' + searchString.replaceAll(' ', '+');
     const topResultLink = inIframe(url, iframe => iframe.contentDocument.querySelector('.search-entry links').href);
+    console.log(topResultLink);
     return inIframe(topResultLink, iframe => {
         const doc = iframe.contentDocument;
-        if(doc.querySelector('.rating-label-true'))         return 'true';
-        if(doc.querySelector('.rating-label-mostly-true'))  return 'mostly-true';
-        if(doc.querySelector('.rating-label-mixture'))      return 'mixture';
-        if(doc.querySelector('.rating-label-mostly-false')) return 'mostly-false';
-        if(doc.querySelector('.rating-label-false'))        return 'false';
-        if(doc.querySelector('.rating-label-unproven'))     return 'unproven';
-        if(doc.querySelector('.rating-label-outdated'))     return 'outdated';
-        if(doc.querySelector('.rating-label-miscaptioned')) return 'miscaptioned';
+        if(doc.querySelector('.rating-label-true'))                return 'true';
+        if(doc.querySelector('.rating-label-mostly-true'))         return 'mostly-true';
+        if(doc.querySelector('.rating-label-mixture'))             return 'mixture';
+        if(doc.querySelector('.rating-label-mostly-false'))        return 'mostly-false';
+        if(doc.querySelector('.rating-label-false'))               return 'false';
+        if(doc.querySelector('.rating-label-unproven'))            return 'unproven';
+        if(doc.querySelector('.rating-label-outdated'))            return 'outdated';
+        if(doc.querySelector('.rating-label-miscaptioned'))        return 'miscaptioned';
         if(doc.querySelector('.rating-label-correct-attribution')) return 'correct-attribution';
-        if(doc.querySelector('.rating-label-misattributed')) return 'misattributed';
-        if(doc.querySelector('.rating-label-scam')) return 'scam';
-        if(doc.querySelector('.rating-label-ledgend')) return 'ledgend';
-        if(doc.querySelector('.rating-label-labeled-satire')) return 'labeled-satire';
-        if(doc.querySelector('.raitng-label-lost-ledgend')) return 'lost-ledgend';
+        if(doc.querySelector('.rating-label-misattributed'))       return 'misattributed';
+        if(doc.querySelector('.rating-label-scam'))                return 'scam';
+        if(doc.querySelector('.rating-label-ledgend'))             return 'ledgend';
+        if(doc.querySelector('.rating-label-labeled-satire'))      return 'labeled-satire';
+        if(doc.querySelector('.raitng-label-lost-legend'))         return 'lost-legend';
 
         return null;
     });
